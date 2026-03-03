5abce863402e7a119a413d1d2ca5a2973ea9f022
Karl Sie <karlchristian11.kc@gmail.com>
2026-03-03 11:49:44 +0400

    refactor

--- airflow/dags/utils/airflow_utils.py
+++ airflow/dags/utils/airflow_utils.py
@@ -43,39 +43,33 @@ def transfer_postgres_to_postgres(**context):
 
 # fetch data from api without pagination
 def load_api_to_postgres(**context):
-    # Runtime parameters (from manual trigger)
     conf = context["dag_run"].conf or {}
     api_url = conf.get("api_url", context["params"]["api_url"])
     target_conn_id = conf.get("target_conn_id", context["params"]["target_conn_id"])
     target_table = conf.get("target_table", context["params"]["target_table"])
 
-    all_rows = []
-    headers = {
-        "Content-Type": "application/json",
-    }
-    response = requests.get(
-        api_url,
-        headers=headers,
-        timeout=60
-    )
-    if response.status_code == 200:
-        data = response.json()
-        rows = data.get("results", [])
-        all_rows.extend(rows)
+    headers = {"Content-Type": "application/json"}
 
-    df = pd.json_normalize(all_rows)
+    response = requests.get(api_url, headers=headers, timeout=60)
+    response.raise_for_status()
+
+    data = response.json()
+    rows = data.get("results", [])
+    df = pd.json_normalize(rows)
 
     if df.empty:
         return
+
+    schema, table = target_table.split(".")
 
     hook = PostgresHook(postgres_conn_id=target_conn_id)
     engine = hook.get_sqlalchemy_engine()
 
     df.to_sql(
-        target_table.split(".")[-1],
-        engine,
-        schema=target_table.split(".")[0],
-        if_exists="append",
+        name=table,
+        con=engine,
+        schema=schema,
+        if_exists="append",  # creates if not exists, appends otherwise
         index=False,
         method="multi",
         chunksize=1000,
